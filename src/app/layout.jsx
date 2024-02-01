a56d2eac6ff0bbd3d1689bddf09d4846f0795e43
sabakhilji <DannyJpr@users.noreply.github.com>
2024-02-01 10:20:44 -0500

    remove case-sensitivity

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 
 import "./globals.css";
-import NavBar from "@/components/NavBar";
+import NavBar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata = {
